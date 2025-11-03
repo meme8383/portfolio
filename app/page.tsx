fbd37e9d33ece832a6d83bc3a21b502572e413be
Eduard Tanase <eduard.tanase1@gmail.com>
2025-11-03 13:18:19 -0500

    noms

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
         Eduard Tanase
       </h1>
       <p className="mb-4">
-        {`I'm Eduard Tanase, a senior at Purdue working on a combined B.S./M.S. in Computer Engineering. My projects bounce between full-stack development, computational neuroscience, and embedded systems.
+        {`I'm Eduard Tanase, a senior at Purdue in Computer Engineering. My projects bounce between full-stack development, computational neuroscience, and embedded systems.
         I build websites like PointOrchard, a credit card comparison site that hit 2,000+ visitors in under a month, and ECELabs, a digital design platform used by every Computer Engineering student at Purdue.
         In my research, I develop machine learning models to interpret mouse brain signals. I enjoy working on challenging projects and learning new things, as well as pursuing hobbies like playing guitar, cooking, and gaming.`}
       </p>
